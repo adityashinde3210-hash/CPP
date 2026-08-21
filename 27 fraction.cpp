9a38d0cd9c655c9adc5efa0dc0b55ea1d3c79e5e
adityashinde3210-hash <adityashinde3210@gmail.com>
2026-08-21 12:53:52 +0530

    Refactor Fraction class and update main function

--- 27 fraction.cpp
+++ 27 fraction.cpp
@@ -1,48 +1,49 @@
 #include<iostream>
 using namespace std;
-class Fraction
+
+class fraction
 {
-   int num,den;
+    int num,den;
+
 public:
-   Fraction(int n=0,int d=1)
+   void input()
    {
-      num=n;
-      den=d;
-   }
-   Fraction add(Fraction f)
-   {
-      return Fraction(num*f.den+f.num*den,den*f.den);
-   }
-   Fraction substract(Fraction f)
-   {
-     return Fraction(num*f.den-f.num*den,den*f.den);
-   }
-   void display()
-   {
-   int a=num,b=den;
-   while(b!=0)
-   {
-      int temp=b;
-      b=a%b;
-      a=temp;
-   }
-   num=num/a;
-   den=den/a;
-   cout<<num<<"/"<<den<<endl;
-   }
+        cout<<"Enter numerator and denominator:";
+        cin>>num>>den;
+    }
+    fraction add(fraction f)
+    {
+        fraction temp;
+        temp.num=(num*f.den)+(f.num*den);
+        temp.den=den*f.den;
+        return temp;    
+    }
+    fraction sub(fraction f)
+    {
+      fraction temp;
+      temp.num=(num*f.den)-(f.num*den);
+      temp.den=den*f.den;
+      return temp;    
+    } 
+    void display()
+    {
+        cout<<num<<"/"<<den;
+    } 
 };
 int main()
 {
-   Fraction f1(3,4),f2(1,4);
-   Fraction addition=f1.add(f2);
-   Fraction substraction=f1.substract(f2);
-   cout<<"First Fraction:";
-   f1.display();
-   cout<<"Second Fraction:";
-   f2.display();
-   cout<<"Addition:";
-   addition.display();
-   cout<<"Substraction:";
-   subtraction.display();
-   return 0;
+    fraction f1,f2,f3;
+
+    f1.input();
+    f2.input();
+
+    f3=f1.add(f2);
+    cout<<"Addition=";
+    f3.display();
+
+    f3=f1.sub(f2);
+    cout<<"\nSubstraction=";
+    f3.display();
+
+    return 0;
 }
